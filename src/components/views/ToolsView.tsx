c71d13fde43e0c3740b2f54627ef577c2d82eb9a
H4K3R-TOOLS <hunterpasha456@gmail.com>
2026-07-09 05:55:14 -0700

    Update ToolsView.tsx

--- src/components/views/ToolsView.tsx
+++ src/components/views/ToolsView.tsx
@@ -33,7 +33,7 @@ export default function ToolsView({ onSelectTool, activeTool }: ToolsViewProps) 
 
     const itemVariants = {
         hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0, transition: { type: 'spring', stiffness: 300, damping: 24 } }
+        visible: { opacity: 1, y: 0, transition: { type: 'spring' as const, stiffness: 300, damping: 24 } }
     };
 
     return (
